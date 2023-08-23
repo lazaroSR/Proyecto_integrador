9f1449d23ab071f7cb65f3a5f26d0bc0c5628785
lazaro silgado <lazarosilgadoaz@gmail.com>
2023-08-23 16:14:15 -0500

    se elaboro un programa de bucle infinito, solo termina presionando la tecla UP

--- main.py
+++ main.py
@@ -2,4 +2,26 @@ print("Bienvenido, Ingresa tu nombre")
 nombre_del_jugador = input()
 print(f"Hola {nombre_del_jugador}, inicias un nuevo reto")
 
-# parte numero 2 del proyecto integrador 
+# parte numero 2 - proyecto integrador 
+import keyboard as kb
+
+n = int(input("Ingresa un numero: "))
+print("Comienza un bucle infinito, detenlo presionando la flecha hacia arriba")
+
+while n > 0:
+  print("Ejecutando..." + str(n))
+  n = n+1
+  if kb.is_pressed("up"):
+    break
+print("FIN")
+
+
+
+
+
+
+
+
+
+
+    
